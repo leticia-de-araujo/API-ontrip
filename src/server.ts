09e5492ca01618b398ea8dba59022ac2500edc1a
th-matheus <matheusth.arte@gmail.com>
2022-09-05 19:24:13 -0300

    feat: application dockerization and welcome route for containerization testing.

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import app from './app';
-import 'dotenv/config';
-import AppDataSource from './data-source';
+import app from "./app";
+import "dotenv/config";
+import AppDataSource from "./data-source";
 
 const init = async () => {
   const PORT = process.env.PORT || 3000;
 
   // --> Precisamos definir as variáveis do .env para inicializar a conexão com o AppDataSource
 
-  //   await AppDataSource.initialize()
-  //     .then(() => {
-  //       console.log('Data Source initialized');
-  //     })
-  //     .catch((err) => {
-  //       console.error('Error during Data Source initialization', err);
-  //     });
+  await AppDataSource.initialize()
+    .then(() => {
+      console.log("Data Source initialized");
+    })
+    .catch((err) => {
+      console.error("Error during Data Source initialization", err);
+    });
 
   app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
