875acd06b091b533e76129503adf141cf101424c
Andre Perregil <andre_perregil@outlook.com>
2022-09-06 19:19:34 +0100

    fix: getting branch up to date with develop (Merge pull-14)

--- src/interfaces/users/index.ts
+++ src/interfaces/users/index.ts
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 export interface IUser {
   id: string;
   username: string;
@@ -23,7 +21,6 @@ export interface IUserNoPassword {
 }
 
 //id gerado automaticamente
->>>>>>> 0a1c8345307d7d7788aa61e50e767e4e03667e52
 export interface IUserRequest {
   username: string;
   email: string;
@@ -33,20 +30,6 @@ export interface IUserRequest {
   photo: string;
 }
 
-<<<<<<< HEAD
-export interface IUserLogin {
-  email: string;
-  password: string;
-}
-
-export interface MyToken {
-  isAdm: boolean;
-  email: string;
-  id: string;
-}
-
-=======
->>>>>>> 0a1c8345307d7d7788aa61e50e767e4e03667e52
 export interface IUserRequestPatch {
   username?: string;
   email?: string;
