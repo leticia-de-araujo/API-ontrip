b996dd585c94dc9880a746d40e5126777f73f7ee
Andre Perregil <andre_perregil@outlook.com>
2022-09-08 11:15:51 +0100

    test: creating tests for /capacities routes, also created aditional mocks required to develop then

--- src/__tests__/mocks/capacityMocks/index.ts
+++ src/__tests__/mocks/capacityMocks/index.ts
@@ -10,6 +10,31 @@ export const mockedCapacity: ICapacityRequest = {
   bathrooms: 1,
 };
 
+export const mockedCapacity2: ICapacityRequest = {
+  rooms: 1,
+  beds: 2,
+  totalGuests: 2,
+  bathrooms: 1,
+};
+
+export const mockedCapacity3: ICapacityRequest = {
+  rooms: 1,
+  beds: 2,
+  totalGuests: 3,
+  bathrooms: 1,
+};
+
+export const mockedCapacityInvalid: ICapacityRequest = {
+  rooms: -1,
+  beds: 1,
+  totalGuests: 0,
+  bathrooms: 1,
+};
+
 export const mockedCapacityPatch: ICapacityRequestPatch = {
   totalGuests: 1,
+};
+
+export const mockedCapacityInvalidPatch: ICapacityRequestPatch = {
+  totalGuests: 0,
 };
