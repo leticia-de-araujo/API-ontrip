c1089df1d06933d17c5926cbf20966ee723af2c6
th-matheus <matheusth.arte@gmail.com>
2022-09-08 16:56:10 -0300

    feat: session login routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 import accommodationsRoutes from "./accommodations.routes";
 import bookingsRoutes from "./bookings.routes";
 import capabilitiesRoutes from "./capabilities.routes";
+import sessionRoutes from "./session.routes";
 import userRoutes from "./users.routes";
 
 const appRoutes = (app: Express) => {
@@ -9,6 +10,7 @@ const appRoutes = (app: Express) => {
   app.use("/accommodations", accommodationsRoutes());
   app.use("/booking", bookingsRoutes());
   app.use("/capabilities", capabilitiesRoutes());
+  app.use("/login", sessionRoutes());
 };
 
 export default appRoutes;
