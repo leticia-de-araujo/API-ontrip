93e41b231858edf4bcc2aa298536e732620f5757
Letícia de Araújo Nunes <leticiadearaujonunes@gmail.com>
2022-09-08 19:34:25 -0300

    Update users.routes.ts

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -12,6 +12,7 @@ const routes = Router();
 //validateUserCreate(userCreateSchema),
 const userRoutes = () => {
   routes.post("", upload.single("files"), userCreateController);
+  routes.get("", listUsersController);
   return routes;
 };
 
