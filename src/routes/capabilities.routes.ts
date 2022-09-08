dbf06b137fb1be559e6b0eefb16d57d4f5dcd265
Hitalo <hitalo.2014.hs@gmail.com>
2022-09-08 14:53:25 -0300

    feat: read capacity route created

--- src/routes/capabilities.routes.ts
+++ src/routes/capabilities.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import createCapacityController from "../controllers/capabilities/createCapacity.controller";
+import readCapabilitiesController from "../controllers/capabilities/readCapabilities.controller";
+import readCapacityController from "../controllers/capabilities/readCapacity.controller";
+import { admValidationMiddleware } from "../middlewares/admValidation.middleware";
 
 const routes = Router();
 
 const capabilitiesRoutes = () => {
-  routes.post("", createCapacityController);
+  routes.post("", admValidationMiddleware, createCapacityController);
+  routes.get("", readCapabilitiesController);
+  routes.get("/:id", readCapacityController);
   return routes;
 };
 
