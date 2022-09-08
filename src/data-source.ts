23068861108b1c544a795407ff2211d267b7c210
André Furtado Volcov <andre.volcov@gmail.com>
2022-09-08 11:59:04 -0300

    fix/depencies

--- src/data-source.ts
+++ src/data-source.ts
@@ -26,15 +26,21 @@ const AppDataSource = new DataSource(
       }
     : {
         type: "postgres",
-        host: process.env.POSTGRES_HOST,
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        logging: true,
+        url: process.env.DATABASE_URL,
+        ssl:
+          process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
         synchronize: false,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
+        logging: true,
+        entities:
+          process.env.NODE_ENV === "production"
+            ? ["dist/entities/.js"]
+            : ["src/entities/.ts"],
+        migrations:
+          process.env.NODE_ENV === "production"
+            ? ["dist/migrations/.js"]
+            : ["src/migrations/.ts"],
       }
 );
 
