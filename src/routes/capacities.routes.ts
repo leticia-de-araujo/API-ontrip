755ddea9f9a2c6058b037476b6d1f3735fc8b448
leticia-de-araujo <leticiadearaujonunes@gmail.com>
2022-09-08 20:44:28 -0300

    fix: capacities read one controller

--- src/routes/capacities.routes.ts
+++ src/routes/capacities.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { admValidationMiddleware } from "../middlewares/admValidation.middleware";
 import capacitiesCreateController from "../controllers/capacities/capacitiesCreate.controller";
 import capacitiesReadAllController from "../controllers/capacities/capacitiesReadAll.controller";
-import capacitiesReadOneController from "../controllers/capacities/capicitiesReadOne.controller";
+import capacitiesReadOneController from "../controllers/capacities/capacitiesReadOne.controller";
 import capacitiesUpdateController from "../controllers/capacities/capacitiesUpdate.controller";
 
 const routes = Router();
