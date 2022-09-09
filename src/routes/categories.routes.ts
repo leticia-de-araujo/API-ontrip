e31bfaed24bde5cf4f94fa7a0dcb4b04f89cce91
Hitalo <hitalo.2014.hs@gmail.com>
2022-09-09 09:56:35 -0300

    feat: list all categories created

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import createCategoryController from "../controllers/categories/createCategory.controller";
+import categoryCreateController from "../controllers/categories/categoryCreate.controller";
+import categoryDeleteController from "../controllers/categories/categoryDelete.controller";
+import categoryReadAllController from "../controllers/categories/categoryReadAll.controller";
 import { admValidationMiddleware } from "../middlewares/admValidation.middleware";
 
 const routes = Router();
 
 const categoriesRouter = () => {
-  routes.post("", admValidationMiddleware, createCategoryController);
+  routes.post("", admValidationMiddleware, categoryCreateController);
+  routes.delete("/:id", admValidationMiddleware, categoryDeleteController);
+  routes.get("", admValidationMiddleware, categoryReadAllController);
+  return routes;
 };
 
 export default categoriesRouter;
