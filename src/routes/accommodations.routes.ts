668c3fb4f2212dd0bc7c1f258e8fb064b03f83a4
Hitalo <hitalo.2014.hs@gmail.com>
2022-09-09 17:14:42 -0300

    feat: update accommodation route created

--- src/routes/accommodations.routes.ts
+++ src/routes/accommodations.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import accommodationCreateController from "../controllers/accommodations/accommodationCreate.controller";
 import accommodationReadAllController from "../controllers/accommodations/accommodationReadAll.controller";
 import accommodationReadOneController from "../controllers/accommodations/accommodationReadOne.controller";
+import accommodationUpdateController from "../controllers/accommodations/accommodationUpdate.controller";
 import { admValidationMiddleware } from "../middlewares/admValidation.middleware";
 import { authUserMiddleware } from "../middlewares/authUser.middleware";
 
@@ -11,6 +12,7 @@ const accommodationsRoutes = () => {
   routes.post("", authUserMiddleware, accommodationCreateController);
   routes.get("", accommodationReadAllController);
   routes.get("/:id", authUserMiddleware, accommodationReadOneController);
+  routes.patch(":/id", authUserMiddleware, accommodationUpdateController);
   return routes;
 };
 
