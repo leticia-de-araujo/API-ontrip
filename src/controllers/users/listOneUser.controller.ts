08987f3e75e130af9db53c23bed24a989182f230
Letícia de Araújo Nunes <leticiadearaujonunes@gmail.com>
2022-09-09 19:09:36 -0300

    Update listOneUser.controller.ts

--- src/controllers/users/listOneUser.controller.ts
+++ src/controllers/users/listOneUser.controller.ts
@@ -7,8 +7,8 @@ const listOneUserController = async (req: Request, res: Response) => {
     const {id} = req.params;
     const user = await listOneUserService(id);
     return res.status(200).json({
-        message: "Success",
-        data: user
+        message: "Successful request",
+        user: user
     })
   } catch (error) {
     if (error instanceof AppError) {
