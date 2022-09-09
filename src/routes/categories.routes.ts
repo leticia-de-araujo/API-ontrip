1c069cf26349ed9173b1a422ad4eb72acf46947f
Hitalo <hitalo.2014.hs@gmail.com>
2022-09-09 10:16:04 -0300

    feat: read one category route created

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import categoryCreateController from "../controllers/categories/categoryCreate.controller";
 import categoryDeleteController from "../controllers/categories/categoryDelete.controller";
 import categoryReadAllController from "../controllers/categories/categoryReadAll.controller";
+import categoryReadOneController from "../controllers/categories/categoryReadOne.controller";
 import { admValidationMiddleware } from "../middlewares/admValidation.middleware";
 
 const routes = Router();
@@ -10,6 +11,7 @@ const categoriesRouter = () => {
   routes.post("", admValidationMiddleware, categoryCreateController);
   routes.delete("/:id", admValidationMiddleware, categoryDeleteController);
   routes.get("", admValidationMiddleware, categoryReadAllController);
+  routes.get("/:id", admValidationMiddleware, categoryReadOneController);
   return routes;
 };
 
