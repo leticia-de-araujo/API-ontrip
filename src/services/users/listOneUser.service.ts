3ad9e1411b0ca415f54322feb4afe8ef117e2912
Bruno <brunopassos_bp@hotmail.com>
2022-09-09 15:34:10 -0300

    feat:list one user feature

--- src/services/users/listOneUser.service.ts
+++ src/services/users/listOneUser.service.ts
@@ -1,8 +1,12 @@
+import { IUserRequest } from './../../interfaces/users/index';
 import AppDataSource from "../../data-source";
 import { User } from "../../entities/users.entity";
 import { AppError } from "../../errors/AppError";
+import app from "../../app";
+import { v2 as cloudinary } from "cloudinary";
 
-const listOneUserService = async (id: string):Promise<User> => {
+const listOneUserService = async (id: string):Promise<IUserRequest> => {
+
     const userRepository = AppDataSource.getRepository(User);
 
     const user = await userRepository.findOne({
@@ -15,7 +19,23 @@ const listOneUserService = async (id: string):Promise<User> => {
         throw new AppError(404, "User not found");
     }
 
-    return user!;
+    const newImg = app.get(`/upload/:id`, (req, res) => {
+        const image = cloudinary.url(user.photo);
+        res.json(image);
+      })
+
+    console.log(newImg);
+
+    const newUser = {
+        username: user.username,
+        email: user.email,
+        password: user.password,
+        dateOfBirth: user.dateOfBirth,
+        isAdm: user.isAdm,
+        photo: newImg
+    }
+
+    return newUser;
 }
 
 export default listOneUserService;
