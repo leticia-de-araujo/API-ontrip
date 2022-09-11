b9f9fac3f7ca7676aab29f00cfa31d70b3b9d6d2
th-matheus <matheusth.arte@gmail.com>
2022-09-11 16:06:21 -0300

    feat: create address controller

--- src/controllers/addresses/createAddress.controller.ts
+++ src/controllers/addresses/createAddress.controller.ts
@@ -1,8 +1,48 @@
 import { Request, Response } from "express";
 import { AppError } from "../../errors/AppError";
+import { IAddressRequest } from "../../interfaces/address";
+import { createAddressService } from "../../services/addresses/createAddress.service";
 
 export const createAddressController = async (req: Request, res: Response) => {
   try {
+    const {
+      country,
+      state,
+      city,
+      postalCode,
+      street,
+      complement,
+      accommodation: accommodationId,
+    } = req.body;
+
+    if (
+      !country ||
+      !state ||
+      !city ||
+      !postalCode ||
+      !street ||
+      !accommodationId
+    ) {
+      throw new AppError(
+        401,
+        "The following fields are required: country, state, city, postalCode, street, accommodation."
+      );
+    }
+
+    const address = await createAddressService({
+      country,
+      state,
+      city,
+      postalCode,
+      street,
+      complement,
+      accommodationId,
+    });
+
+    return res.status(201).json({
+      message: "success",
+      data: address,
+    });
   } catch (err) {
     if (err instanceof AppError) {
       throw new AppError(err.statusCode, err.message);
