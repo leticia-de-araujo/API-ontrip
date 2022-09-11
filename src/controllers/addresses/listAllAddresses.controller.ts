58fbe2518783377e148591021b4ccc283ff6680b
th-matheus <matheusth.arte@gmail.com>
2022-09-11 17:06:13 -0300

    feat: list all addresses route

--- src/controllers/addresses/listAllAddresses.controller.ts
+++ src/controllers/addresses/listAllAddresses.controller.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from "express";
 import { AppError } from "../../errors/AppError";
+import { listAllAddressesService } from "../../services/addresses/listAllAddresses.service";
 
 export const listAllAddressesController = async (
   req: Request,
   res: Response
 ) => {
   try {
+    const addresses = await listAllAddressesService();
+
+    return res.status(200).json({
+      message: "success",
+      data: addresses,
+    });
   } catch (err) {
     if (err instanceof AppError) {
       throw new AppError(err.statusCode, err.message);
