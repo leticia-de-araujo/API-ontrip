6870e6f8049c78c14a78b4d241b7a95d013d1f30
th-matheus <matheusth.arte@gmail.com>
2022-09-11 21:48:15 -0300

    refactor: removing address listing from routes

--- src/routes/addresses.routes.ts
+++ src/routes/addresses.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { createAddressController } from "../controllers/addresses/createAddress.controller";
-import { listAllAddressesController } from "../controllers/addresses/listAllAddresses.controller";
 import { updateAddressController } from "../controllers/addresses/updateAddress.controller";
 import { admOrOwnerAuthMiddleware } from "../middlewares/admOrOwnerAuth.middleware";
 import { authUserMiddleware } from "../middlewares/authUser.middleware";
@@ -15,7 +14,6 @@ const addressesRoutes = () => {
     admOrOwnerAuthMiddleware /* adicionar lógica no middleware para aceitar a rota de address */,
     updateAddressController
   );
-  routes.get("", authUserMiddleware, listAllAddressesController);
 
   return routes;
 };
