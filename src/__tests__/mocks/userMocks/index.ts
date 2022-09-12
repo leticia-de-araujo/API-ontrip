fe1532ac16015c2eb738a289a30583524380a230
Hitalo <hitalo.2014.hs@gmail.com>
2022-09-12 15:31:43 -0300

    fix: fixed data format from mocks

--- src/__tests__/mocks/userMocks/index.ts
+++ src/__tests__/mocks/userMocks/index.ts
@@ -4,7 +4,7 @@ export const mockedUser: IUserRequest = {
   username: "Hitalo",
   email: "hitalo@mail.com",
   password: "123456",
-  dateOfBirth: "2000/02/11",
+  dateOfBirth: "2000-02-11",
   isAdm: false,
 };
 
@@ -12,7 +12,7 @@ export const mockedUser2: IUserRequest = {
   username: "Marquinhos Atalaia",
   email: "hitalo@mail.com",
   password: "123456",
-  dateOfBirth: "1999/02/11",
+  dateOfBirth: "1999-02-11",
   isAdm: false,
 };
 
@@ -20,7 +20,7 @@ export const mockedUser3: IUserRequest = {
   username: "Marquinhos Atalaia",
   email: "marquinhos@mail.com",
   password: "123456",
-  dateOfBirth: "1999/02/11",
+  dateOfBirth: "1999-02-11",
   isAdm: false,
 };
 
@@ -35,7 +35,7 @@ export const mockedAdmin: IUserRequest = {
   username: "Andre",
   email: "andre@mail.com",
   password: "123456",
-  dateOfBirth: "1993/02/07",
+  dateOfBirth: "1993-02-07",
   isAdm: true,
 };
 
@@ -58,6 +58,6 @@ export const mockedUserWrongType: any = {
   username: "Hitalo",
   email: "hitalo@mail.com",
   password: 123456,
-  dateOfBirth: "2000/02/11",
+  dateOfBirth: "2000-02-11",
   isAdm: "false",
 };
