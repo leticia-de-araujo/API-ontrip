269fb89cb2671cd327ad1311fbc07b858e15d446
th-matheus <matheusth.arte@gmail.com>
2022-09-11 21:20:36 -0300

    feat: update address route

--- src/routes/addresses.routes.ts
+++ src/routes/addresses.routes.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
 import { createAddressController } from "../controllers/addresses/createAddress.controller";
 import { listAllAddressesController } from "../controllers/addresses/listAllAddresses.controller";
+import { updateAddressController } from "../controllers/addresses/updateAddress.controller";
+import { admOrOwnerAuthMiddleware } from "../middlewares/admOrOwnerAuth.middleware";
 import { authUserMiddleware } from "../middlewares/authUser.middleware";
 
 const routes = Router();
 
 const addressesRoutes = () => {
   routes.post("", authUserMiddleware, createAddressController);
+  routes.patch(
+    "/:addressId",
+    authUserMiddleware,
+    admOrOwnerAuthMiddleware /* adicionar lógica no middleware para aceitar a rota de address */,
+    updateAddressController
+  );
   routes.get("", authUserMiddleware, listAllAddressesController);
 
   return routes;
