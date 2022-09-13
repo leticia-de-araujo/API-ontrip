f44e02c845ad57211a4b19d20ca134769e46aef2
th-matheus <matheusth.arte@gmail.com>
2022-09-13 20:31:02 -0300

    fix: users controllers

--- src/controllers/users/listOneUser.controller.ts
+++ src/controllers/users/listOneUser.controller.ts
@@ -6,7 +6,9 @@ import listOneUserService from "../../services/users/listOneUser.service";
 const listOneUserController = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
+
     const user = await listOneUserService(id);
+
     return res.status(200).json({
       message: "Successful request",
       user: instanceToPlain(user),
