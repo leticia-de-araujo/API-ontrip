766b80c517075f8cad01148b004c1ed3541ad5cd
Hitalo <hitalo.2014.hs@gmail.com>
2022-09-12 21:01:25 -0300

    fix: users request syntax fixed patch

--- src/controllers/users/updateUser.controller.ts
+++ src/controllers/users/updateUser.controller.ts
@@ -4,19 +4,33 @@ import updateUserService from "../../services/users/updateUser.service";
 import { IUserRequest } from "../../interfaces/users";
 
 const updateUserController = async (req: Request, res: Response) => {
-    try {
-        const {id} = req.params;
-        const {username, email, password, dateOfBirth, isAdm, photo}:IUserRequest = req.body;
-        const user = await updateUserService(id, {username, email, password, dateOfBirth, isAdm, photo});
-        return res.status(200).json({
-            message: "User updated",
-            data: user
-        })
-    } catch (error) {
-        if(error instanceof AppError){
-            throw new AppError(error.statusCode, error.message);
-        }
+  try {
+    const { id } = req.params;
+    const {
+      username,
+      email,
+      password,
+      dateOfBirth,
+      isAdm,
+      photo,
+    }: IUserRequest = req.body;
+    const user = await updateUserService(id, {
+      username,
+      email,
+      password,
+      dateOfBirth,
+      isAdm,
+      photo,
+    });
+    return res.status(200).json({
+      message: "User updated",
+      user: user,
+    });
+  } catch (error) {
+    if (error instanceof AppError) {
+      throw new AppError(error.statusCode, error.message);
     }
-}
+  }
+};
 
 export default updateUserController;
