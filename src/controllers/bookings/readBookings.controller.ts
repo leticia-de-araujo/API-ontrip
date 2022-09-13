f452db41bcdd536d30405e2896b3a857d81fe145
Letícia de Araújo Nunes <leticiadearaujonunes@gmail.com>
2022-09-13 12:40:02 -0300

    Update readBookings.controller.ts

--- src/controllers/bookings/readBookings.controller.ts
+++ src/controllers/bookings/readBookings.controller.ts
@@ -5,7 +5,7 @@ import readBookingsService from "../../services/bookings/readBookings.service";
 const readBookingsController = async (req: Request, res: Response) => {
   try {
     const readBookings = await readBookingsService();
-    return res.json({ message: "Success", bookings: readBookings });
+    return res.json({ message: "Successful request", bookings: readBookings });
   } catch (error) {
     if (error instanceof AppError) {
       throw new AppError(error.statusCode, error.message);
