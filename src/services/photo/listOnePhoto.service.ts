28d6ee1195af4a25047f0076f3fc41b1aebc8d47
th-matheus <matheusth.arte@gmail.com>
2022-09-13 20:21:32 -0300

    fix: list one photo controller

--- src/services/photo/listOnePhoto.service.ts
+++ src/services/photo/listOnePhoto.service.ts
@@ -3,6 +3,10 @@ import { Photo } from "../../entities/photo.entity";
 import { AppError } from "../../errors/AppError";
 
 export const listOnePhotoService = async (photoId: string) => {
+  if (!photoId) {
+    throw new AppError(400, "photoId is missing");
+  }
+
   const photoRepository = AppDataSource.getRepository(Photo);
   const photo = await photoRepository.findOneBy({ id: photoId });
 
