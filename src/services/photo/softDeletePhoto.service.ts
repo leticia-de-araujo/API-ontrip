bcd46635a978f2233c7ef89181ec0cac32ce7d34
th-matheus <matheusth.arte@gmail.com>
2022-09-13 20:22:25 -0300

    fix: soft delete photo controller

--- src/services/photo/softDeletePhoto.service.ts
+++ src/services/photo/softDeletePhoto.service.ts
@@ -3,6 +3,10 @@ import { AppError } from "../../errors/AppError";
 import { Photo } from "../../entities/photo.entity";
 
 export const softDeletePhotoService = async (photoId: string) => {
+  if (!photoId) {
+    throw new AppError(400, "photoId is missing");
+  }
+
   const photoRepository = AppDataSource.getRepository(Photo);
 
   const photo = await photoRepository.findOneBy({ id: photoId });
